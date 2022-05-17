62cf9beb9464b325086dbd0cb725d2753d7a9485
Roman Chaus <roman1997lviv@gmail.com>
2022-05-18 00:01:23 +0300

    js functions added

--- src/index.js
+++ src/index.js
@@ -7,24 +7,11 @@ function scrollFunction1() {
     });
   }
 
-function thanos() {
-    
-}
-disintegrate.init()
-
-window.addEventListener("scroll", e => {
-    let elementTarget = document.getElementById("disappear");
-    if (window.scrollY > (elementTarget.offsetTop)) {
-        const disObj = disintegrate.getDisObj(e.target)
-      disintegrate.createSimultaneousParticles(disObj)
-      e.target.removeEventListener();
+  window.addEventListener("scroll", function() {
+    let elementTarget = document.getElementById("thanos-disappear");
+    if (window.scrollY > (elementTarget.offsetTop - 100)) {
+        elementTarget.classList.add("hidden"); ;
     }
   });
 
-  disintegrate.init()
-
-  document.getElementById("disappear").addEventListener("scroll", e => {
-      const disObj = disintegrate.getDisObj(e.target)
-      disintegrate.createSimultaneousParticles(disObj)
-      e.target.removeEventListener()
-  })
+  
